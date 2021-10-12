ab7a04d5a06eccb0d706c65f6db197001361630d
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-12 05:26:22 +0200

    ⚡️ navigation and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section id="hero">
+      <StaticImage src="../images/home.png" alt="hero" />
+      <div className="hero_stuff text-center">
+        <h1 className="hero_title display-2">The Blog</h1>
+        <h2 className="hero_subtitle display-5">Write Ideas</h2>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,35 @@
+import { Link } from "gatsby";
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../images/logo.png";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <Navbar id="mainNav" fixed="top" bg="light" expand="lg">
+      <Container>
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="BlogIt!" />
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+            <Link className="nav-link" to="/posts">
+              Posts
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
