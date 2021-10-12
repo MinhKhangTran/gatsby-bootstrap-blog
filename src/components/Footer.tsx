26d752a3d66e0786e062270550ab8f98b5dfe585
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-12 05:47:04 +0200

    ✨ Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { FaTwitter, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="socials d-flex justify-content-center">
+        <a href="#">
+          <FaTwitter size="2em" />
+        </a>
+        <a href="#">
+          <FaGithub size="2em" />
+        </a>
+        <a href="#">
+          <FaInstagram size="2em" />
+        </a>
+      </div>
+      <div className="footer text-center">
+        <p className="text-teal-400">
+          Copyright &copy; BlogIt! {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
