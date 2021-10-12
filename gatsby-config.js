8671f8aa7ab77ee396e2c1c120104524ef02c4b4
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-12 04:52:40 +0200

    :tada: start new project with cms

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gatsby_bootstrap_custom_portfolio",
+    description: "A static site made with gatsby and bootstrap ✨",
+    keywords: "bootstrap, gatsby",
+    author: "mkt",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-graphcms",
+      options: {
+        endpoint: process.env.GRAPHCMS_ENDPOINT,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
